0a047efb7b8d2ed6940acd41491aefea4e67fbcb
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-06-10 15:17:23 +0200

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,10 +4,10 @@ import asyncio
 import random
 
 
-async def async_generator(max_delay) -> float:
+async def async_random(max_dela: float = 10.0) -> float:
     
     '''Randomise a sequence of 10 numbers. '''
- delay = generator.uniform(0, max_delay) 
+ delay = random.uniform(0, max_delay) 
         await asyncio.sleep(1)
         return delay
       
