b087a48f176be13e1a5ad50432f1e31dd794f8bd
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-06-10 15:10:36 +0200

    Create 0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''task0.'''
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''Randomise a sequence of 10 numbers. '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
